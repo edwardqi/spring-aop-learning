79e2098ce29974b73c8f10a3e877323181d601bc
zhili.qi <zhili.qi@microfocus.com>
2020-01-19 17:41:18 +0800

    initial commit

---
+++ src/main/java/com/ed/springaoplearning/SpringaoplearningApplication.java
@@ -0,0 +1,13 @@
+package com.ed.springaoplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaoplearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaoplearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ed/springaoplearning/SpringaoplearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.springaoplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaoplearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
